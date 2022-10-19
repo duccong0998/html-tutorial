27e4e43edadcdb3ce93f2a62a4d683c9e2ba14a1
duccong0998 <114991557+duccong0998@users.noreply.github.com>
2022-10-19 08:29:38 +0700

    hoan thanh

---
+++ index3.js
@@ -0,0 +1,6 @@
+function chuvihv() {
+    let a = document.getElementById("ip").value;
+    let chuvi = a * 4;
+    let dientich = a * a;
+    document.getElementById("inraday").innerHTML ="Chu vi:" + chuvi + "<br>" + "Diện tích:" + dientich ;
+}
